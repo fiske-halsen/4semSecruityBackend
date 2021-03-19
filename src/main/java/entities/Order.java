872178407bfd81b33364c89c4f4182ef17fe762e
Phillip Andersen <phillip_andersen@hotmail.com>
2021-03-19 12:31:23 +0100

    working on the entities

---
+++ src/main/java/entities/Order.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+    
+    @ManyToMany(cascade = CascadeType.PERSIST, mappedBy = "orders")
+    private List<Product> products;
+
+    public Order() {
+        this.products = new ArrayList();
+    }
+    
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        if(product != null){
+            this.products.add(product);
+        }
+    }
+    
+}
